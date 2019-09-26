3385df4be9d6989ce793b310b9759f9e2748fefd
Mido <supermido1996@gmail.com>
2019-09-26 16:45:11 +0700

    Compete all lab

---
+++ OOP/Inheritance/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("barking...");
+        }
+    }
+
+    public class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+
+    public class RandomList : List<string>
+    {
+        private Random randomElement;
+
+        public string RandomString()
+        {
+            int pos;
+            pos = randomElement.Next(0, Count);
+            string element = this[pos];
+            RemoveAt(pos);
+            return element;
+        }
+    }
+
+    public class StackOfStrings
+    {
+        private List<string> data;
+        public void Push(string item)
+        {
+            data.Add(item);
+        }
+
+        public string Pop()
+        {
+            string item = data[data.Count -1];
+            data.RemoveAt(data.Count -1);
+            return item;
+        }
+
+        public string Peek()
+        {
+            return data[data.Count -1];
+        }
+
+        public bool IsEmpty()
+        {
+            if(data.Count ==0) return true;
+            else return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Eat();
+            myDog.Bark();
+
+            // Puppy myPyppy = new Puppy();
+            // myPyppy.Eat();
+            // myPyppy.Bark();
+            // myPyppy.Weep();
+
+            Cat myCat = new Cat();
+            myCat.Eat();
+            myCat.Meow();
+        }
+    }
+}
---
+++ OOP/Polymorphism/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    public class MathOperations
+    {
+        public int Add(int a, int b)
+        {
+            return a +b;
+        }
+
+        public double Add(double a, double b, double c)
+        {
+            return a+b+c;
+        }
+
+        public decimal Add(decimal a, decimal b, decimal c)
+        {
+            return a+b+c;
+        }
+    }
+
+    public class Animal
+    {
+        private string name;
+        private string favoriteFood;
+
+        public Animal(string name, string favoriteFood)
+        {
+            this.name = name;
+            this.favoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return ($"I am {this.name} and my fovourite food is {this.favoriteFood}");
+        }
+
+    }
+
+    public class Cat : Animal
+    {
+        public Cat(string Name, string FavouriteFood) : base(Name, FavouriteFood)
+        {
+
+        }
+
+        public override string ExplainSelf()
+        {
+
+            return base.ExplainSelf() + "\nMEEOW";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(string Name, string FavoriteFood) : base (Name, FavoriteFood)
+        {
+
+        }
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() + "\nDJAFF";
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+        public abstract double CalculateArea();
+        public virtual string Draw()
+        {
+            return "...";
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public double Height
+        {
+            get {return height; }
+            set { height = value; }
+        }
+
+        public double Width
+        {
+            get {return width;}
+            set {width = value;}
+        }
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (Width+Height)*2;
+        }
+
+        public override double CalculateArea()
+        {
+            return Width*Height;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+        public double Radius
+        {
+            get {return radius;}
+            set {radius = value;}
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Radius * 2 * 3.14;
+        }
+
+        public override double CalculateArea()
+        {
+            return Radius * Radius * 3.14;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MathOperations myMathOperations = new MathOperations();
+            // Console.WriteLine(myMathOperations.Add(2,3));
+            // Console.WriteLine(myMathOperations.Add(2.2,3.3,5.5));
+            // Console.WriteLine(myMathOperations.Add(2.2m, 3.3m, 4.4m));
+            // Animal cat = new Cat("Pesho", "Whiskas");
+            // Animal dog = new Dog("Gosho", "Meet");
+
+            // Console.WriteLine(cat.ExplainSelf());
+            // Console.WriteLine(dog.ExplainSelf());
+
+            Shape myRectangle = new Rectangle(3,5);
+            Shape myCircle = new Circle(5);
+
+            Console.Write("Area of Circle: ");
+            Console.WriteLine(myCircle.CalculateArea());
+            Console.Write("Perimeter of Circle: ");
+            Console.WriteLine(myCircle.CalculatePerimeter());
+
+            Console.WriteLine();
+
+            Console.Write("Area of Rectangle: ");
+            Console.WriteLine(myRectangle.CalculateArea());
+            Console.Write("Perimeter of Rectangle: ");
+            Console.WriteLine(myRectangle.CalculatePerimeter());
+
+            
+        }
+    }
+}
