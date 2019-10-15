0a8c1add1e0688339d20d4ccc67d0ad1138d2801
Mido <supermido1996@gmail.com>
2019-10-16 00:15:25 +0700

    add Design Pattern

---
+++ Design-Pattern/Builder/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class House
+    {
+        private int mFloors;
+        private bool mLivingRoom;
+        private bool mBedRoom;
+        private bool mBathRoom;
+        private bool mSwimmingPool;
+
+        public House (HouseBuilder builder)
+        {
+            this.mFloors = builder.Floors;
+            this.mLivingRoom = builder.LivingRoom;
+            this.mBedRoom = builder.BedRoom;
+            this.mBathRoom = builder.BathRoom;
+            this.mSwimmingPool = builder.SwimmingPool;
+        }
+
+        public string GetDescription()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"The house has {this.mFloors} floor(s).\n");
+            sb.Append("---------------------------\n");
+            sb.Append($"Living Room: \t{this.mLivingRoom}\n");
+            sb.Append($"Bed Room: \t{this.mBedRoom}\n");
+            sb.Append($"Bath Room: \t{this.mBathRoom}\n");
+            sb.Append($"Swimming Pool: \t{this.mSwimmingPool}");
+            return sb.ToString();
+        }
+    }
+
+    class HouseBuilder
+    {
+        public int Floors;
+        public bool LivingRoom;
+        public bool BedRoom;
+        public bool BathRoom;
+        public bool SwimmingPool;
+
+        public HouseBuilder (int floor)
+        {
+            this.Floors = floor;
+        }
+
+        public HouseBuilder AddLivingRoom()
+        {
+            this.LivingRoom = true;
+            return this;
+        }
+
+        public HouseBuilder AddBedRoom()
+        {
+            this.BedRoom = true;
+            return this;
+        }
+
+        public HouseBuilder AddBathRoom()
+        {
+            this.BathRoom = true;
+            return this;
+        }
+
+        public HouseBuilder AddSwimmingPool()
+        {
+            this.SwimmingPool = true;
+            return this;
+        }
+
+        public House Build()
+        {
+            return new House(this);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myHouse = new HouseBuilder(3).AddBathRoom()
+                                             .AddBedRoom()
+                                            //  .AddLivingRoom()
+                                             .AddSwimmingPool()
+                                             .Build();
+
+            Console.WriteLine(myHouse.GetDescription());
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Design-Pattern/Decorator/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Decorator
+{
+    interface IMacbook
+    {
+        int GetCost();
+        int GetRam();
+        string GetChip();
+        string GetDescription();
+    }
+
+    class MacAir : IMacbook
+    {
+        public int GetCost()
+        {
+            return 720;
+        }
+
+        public int GetRam()
+        {
+            return 4;
+        }
+
+        public string GetChip()
+        {
+            return "i3";
+        }
+
+        public string GetDescription()
+        {
+            return $"Macbook Air: {GetChip()}\t\tRam: {GetRam()}\t\tCost: {GetCost()}";
+        }
+    }
+
+    class MacNew : IMacbook
+    {
+        private readonly IMacbook mMacbook;
+
+        public MacNew(IMacbook macbook)
+        {
+            mMacbook = macbook;
+        }
+
+        public int GetCost()
+        {
+            return mMacbook.GetCost() + 190;
+        }
+
+        public int GetRam()
+        {
+            return mMacbook.GetRam() + 4;
+        }
+
+        public string GetChip()
+        {
+            return "i5";
+        }
+
+        public string GetDescription()
+        {
+            return $"Macbook New: {GetChip()}\t\tRam: {GetRam()}\t\tCost: {GetCost()}";
+        }
+    }
+
+    class MacPro : IMacbook
+    {
+        private readonly IMacbook mMacbook;
+
+        public MacPro(IMacbook macbook)
+        {
+            mMacbook = macbook;
+        }
+
+        public int GetCost()
+        {
+            return mMacbook.GetCost() + 320;
+        }
+
+        public int GetRam()
+        {
+            return mMacbook.GetRam() + 8;
+        }
+
+        public string GetChip()
+        {
+            return "i7";
+        }
+
+        public string GetDescription()
+        {
+            return $"Macbook Pro: {GetChip()}\t\tRam: {GetRam()}\t\tCost: {GetCost()}";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================== Macbook Shop =====================");
+            var myMacAir = new MacAir();
+            Console.WriteLine(myMacAir.GetDescription());
+
+            var myMacNew = new MacNew(myMacAir);
+            Console.WriteLine(myMacNew.GetDescription());
+
+            var myMacPro = new MacPro(myMacNew);
+            Console.WriteLine(myMacPro.GetDescription());
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
